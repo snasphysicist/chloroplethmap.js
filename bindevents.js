fb2ec770ae0b8c4773aaddd62dbc2913633438e1
snasphysicist <ih8coldplay@hotmail.com>
2018-07-19 09:06:43 +0100

    Added a select for the super region (UK/US/etc), a variable to hold the selection and code to ensure that the variable is updated when the selection is updated.

--- bindevents.js
+++ bindevents.js
@@ -1,4 +1,11 @@
 
+/*
+ * This file handles "initialisation"
+ * for most things (currently)
+ * Including binding events, setting up
+ * list boxes dynamically, etc...
+ */
+
 //Add routine to generate heatmap
 //to "Generate" button
 const GENERATE_BUTTON = document.querySelector( ".heatmap-button" ) ;
@@ -13,3 +20,32 @@ COPYRIGHT_TEXT.addEventListener( "click" , showModal ) ;
 //to whole page
 const CLOSE_BUTTON = document.querySelector( ".close-button" ) ;
 CLOSE_BUTTON.addEventListener( "click" , hideModal ) ;
+
+/*
+ * Set up the superregion options in the drop down box
+ * i.e. UK/US/World/etc
+ * note: Depends upon mapData variable from chloroplethmap.js
+ */
+const REGION_SELECTOR = document.querySelector( "#super-region-selector" ) ;
+
+for( superRegion in mapData.getAllSuperregionIdentifiers() ) {
+  //Create a new option element for each
+  let newOption = document.createElement( "option" ) ;
+  newOption.setAttribute( "value" , mapData.getAllSuperregionIdentifiers()[ superRegion ].toLowerCase() ) ;
+  newOption.innerHTML = mapData.getAllSuperregionIdentifiers()[ superRegion ] ;
+  REGION_SELECTOR.appendChild( newOption ) ;
+}
+
+/*
+ * Updates the value stored in the superRegion variable
+ * (the currently selected superregion)
+ * note: Depends on the superRegion varible from chloroplethmap.js
+ */
+function updateSuperRegion() {
+  superRegion = REGION_SELECTOR.value ;
+}
+//Run it once to ensure that the variable has the default value1
+updateSuperRegion() ;
+
+//Run the function when the superregion selection changes
+REGION_SELECTOR.addEventListener( "change" , updateSuperRegion ) ;
